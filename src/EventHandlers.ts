cc4b95df999f9b7e62a7ba143b9821d29ede5470
sekuba <29250140+sekuba@users.noreply.github.com>
2025-08-03 10:08:14 +0200

    iniiiiit

--- src/EventHandlers.ts
+++ src/EventHandlers.ts
@@ -3,13 +3,62 @@
  */
 import {
   L1AssetRouter,
+  L1AssetRouter_AssetDeploymentTrackerRegistered,
+  L1AssetRouter_AssetDeploymentTrackerSet,
+  L1AssetRouter_AssetHandlerRegistered,
   L1AssetRouter_BridgehubDepositBaseTokenInitiated,
   L1AssetRouter_BridgehubDepositFinalized,
   L1AssetRouter_BridgehubDepositInitiated,
+  L1AssetRouter_BridgehubMintData,
+  L1AssetRouter_BridgehubWithdrawalInitiated,
   L1AssetRouter_ClaimedFailedDepositAssetRouter,
   L1AssetRouter_DepositFinalizedAssetRouter,
   L1AssetRouter_LegacyDepositInitiated,
+  L1AssetRouter_Paused,
+  L1AssetRouter_Unpaused,
+  L2AssetRouter,
+  L2AssetRouter_AssetDeploymentTrackerRegistered,
+  L2AssetRouter_AssetHandlerRegistered,
+  L2AssetRouter_BridgehubDepositBaseTokenInitiated,
+  L2AssetRouter_BridgehubDepositInitiated,
+  L2AssetRouter_BridgehubWithdrawalInitiated,
+  L2AssetRouter_DepositFinalizedAssetRouter,
+  L2AssetRouter_Paused,
+  L2AssetRouter_Unpaused,
+  L2AssetRouter_WithdrawalInitiatedAssetRouter,
 } from "generated";
+
+L1AssetRouter.AssetDeploymentTrackerRegistered.handler(async ({ event, context }) => {
+  const entity: L1AssetRouter_AssetDeploymentTrackerRegistered = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    assetId: event.params.assetId,
+    additionalData: event.params.additionalData,
+    assetDeploymentTracker: event.params.assetDeploymentTracker,
+  };
+
+  context.L1AssetRouter_AssetDeploymentTrackerRegistered.set(entity);
+});
+
+L1AssetRouter.AssetDeploymentTrackerSet.handler(async ({ event, context }) => {
+  const entity: L1AssetRouter_AssetDeploymentTrackerSet = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    assetId: event.params.assetId,
+    assetDeploymentTracker: event.params.assetDeploymentTracker,
+    additionalData: event.params.additionalData,
+  };
+
+  context.L1AssetRouter_AssetDeploymentTrackerSet.set(entity);
+});
+
+L1AssetRouter.AssetHandlerRegistered.handler(async ({ event, context }) => {
+  const entity: L1AssetRouter_AssetHandlerRegistered = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    assetId: event.params.assetId,
+    _assetHandlerAddress: event.params._assetHandlerAddress,
+  };
+
+  context.L1AssetRouter_AssetHandlerRegistered.set(entity);
+});
 
 L1AssetRouter.BridgehubDepositBaseTokenInitiated.handler(async ({ event, context }) => {
   const entity: L1AssetRouter_BridgehubDepositBaseTokenInitiated = {
@@ -47,6 +96,27 @@ L1AssetRouter.BridgehubDepositInitiated.handler(async ({ event, context }) => {
   context.L1AssetRouter_BridgehubDepositInitiated.set(entity);
 });
 
+L1AssetRouter.BridgehubMintData.handler(async ({ event, context }) => {
+  const entity: L1AssetRouter_BridgehubMintData = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    bridgeMintData: event.params.bridgeMintData,
+  };
+
+  context.L1AssetRouter_BridgehubMintData.set(entity);
+});
+
+L1AssetRouter.BridgehubWithdrawalInitiated.handler(async ({ event, context }) => {
+  const entity: L1AssetRouter_BridgehubWithdrawalInitiated = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    chainId: event.params.chainId,
+    sender: event.params.sender,
+    assetId: event.params.assetId,
+    assetDataHash: event.params.assetDataHash,
+  };
+
+  context.L1AssetRouter_BridgehubWithdrawalInitiated.set(entity);
+});
+
 L1AssetRouter.ClaimedFailedDepositAssetRouter.handler(async ({ event, context }) => {
   const entity: L1AssetRouter_ClaimedFailedDepositAssetRouter = {
     id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
@@ -81,4 +151,121 @@ L1AssetRouter.LegacyDepositInitiated.handler(async ({ event, context }) => {
   };
 
   context.L1AssetRouter_LegacyDepositInitiated.set(entity);
+});
+
+L1AssetRouter.Paused.handler(async ({ event, context }) => {
+  const entity: L1AssetRouter_Paused = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    account: event.params.account,
+  };
+
+  context.L1AssetRouter_Paused.set(entity);
+});
+
+L1AssetRouter.Unpaused.handler(async ({ event, context }) => {
+  const entity: L1AssetRouter_Unpaused = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    account: event.params.account,
+  };
+
+  context.L1AssetRouter_Unpaused.set(entity);
+});
+
+L2AssetRouter.AssetDeploymentTrackerRegistered.handler(async ({ event, context }) => {
+  const entity: L2AssetRouter_AssetDeploymentTrackerRegistered = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    assetId: event.params.assetId,
+    additionalData: event.params.additionalData,
+    assetDeploymentTracker: event.params.assetDeploymentTracker,
+  };
+
+  context.L2AssetRouter_AssetDeploymentTrackerRegistered.set(entity);
+});
+
+L2AssetRouter.AssetHandlerRegistered.handler(async ({ event, context }) => {
+  const entity: L2AssetRouter_AssetHandlerRegistered = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    assetId: event.params.assetId,
+    _assetHandlerAddress: event.params._assetHandlerAddress,
+  };
+
+  context.L2AssetRouter_AssetHandlerRegistered.set(entity);
+});
+
+L2AssetRouter.BridgehubDepositBaseTokenInitiated.handler(async ({ event, context }) => {
+  const entity: L2AssetRouter_BridgehubDepositBaseTokenInitiated = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    chainId: event.params.chainId,
+    from: event.params.from,
+    assetId: event.params.assetId,
+    amount: event.params.amount,
+  };
+
+  context.L2AssetRouter_BridgehubDepositBaseTokenInitiated.set(entity);
+});
+
+L2AssetRouter.BridgehubDepositInitiated.handler(async ({ event, context }) => {
+  const entity: L2AssetRouter_BridgehubDepositInitiated = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    chainId: event.params.chainId,
+    txDataHash: event.params.txDataHash,
+    from: event.params.from,
+    assetId: event.params.assetId,
+    bridgeMintCalldata: event.params.bridgeMintCalldata,
+  };
+
+  context.L2AssetRouter_BridgehubDepositInitiated.set(entity);
+});
+
+L2AssetRouter.BridgehubWithdrawalInitiated.handler(async ({ event, context }) => {
+  const entity: L2AssetRouter_BridgehubWithdrawalInitiated = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    chainId: event.params.chainId,
+    sender: event.params.sender,
+    assetId: event.params.assetId,
+    assetDataHash: event.params.assetDataHash,
+  };
+
+  context.L2AssetRouter_BridgehubWithdrawalInitiated.set(entity);
+});
+
+L2AssetRouter.DepositFinalizedAssetRouter.handler(async ({ event, context }) => {
+  const entity: L2AssetRouter_DepositFinalizedAssetRouter = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    chainId: event.params.chainId,
+    assetId: event.params.assetId,
+    assetData: event.params.assetData,
+  };
+
+  context.L2AssetRouter_DepositFinalizedAssetRouter.set(entity);
+});
+
+L2AssetRouter.Paused.handler(async ({ event, context }) => {
+  const entity: L2AssetRouter_Paused = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    account: event.params.account,
+  };
+
+  context.L2AssetRouter_Paused.set(entity);
+});
+
+L2AssetRouter.Unpaused.handler(async ({ event, context }) => {
+  const entity: L2AssetRouter_Unpaused = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    account: event.params.account,
+  };
+
+  context.L2AssetRouter_Unpaused.set(entity);
+});
+
+L2AssetRouter.WithdrawalInitiatedAssetRouter.handler(async ({ event, context }) => {
+  const entity: L2AssetRouter_WithdrawalInitiatedAssetRouter = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    chainId: event.params.chainId,
+    l2Sender: event.params.l2Sender,
+    assetId: event.params.assetId,
+    assetData: event.params.assetData,
+  };
+
+  context.L2AssetRouter_WithdrawalInitiatedAssetRouter.set(entity);
 });
--- test/Test.ts
+++ test/Test.ts
@@ -1,38 +1,37 @@
 import assert from "assert";
 import { 
   TestHelpers,
-  L1AssetRouter_BridgehubDepositBaseTokenInitiated
+  L1AssetRouter_AssetDeploymentTrackerRegistered
 } from "generated";
 const { MockDb, L1AssetRouter } = TestHelpers;
 
-describe("L1AssetRouter contract BridgehubDepositBaseTokenInitiated event tests", () => {
+describe("L1AssetRouter contract AssetDeploymentTrackerRegistered event tests", () => {
   // Create mock db
   const mockDb = MockDb.createMockDb();
 
-  // Creating mock for L1AssetRouter contract BridgehubDepositBaseTokenInitiated event
-  const event = L1AssetRouter.BridgehubDepositBaseTokenInitiated.createMockEvent({/* It mocks event fields with default values. You can overwrite them if you need */});
+  // Creating mock for L1AssetRouter contract AssetDeploymentTrackerRegistered event
+  const event = L1AssetRouter.AssetDeploymentTrackerRegistered.createMockEvent({/* It mocks event fields with default values. You can overwrite them if you need */});
 
-  it("L1AssetRouter_BridgehubDepositBaseTokenInitiated is created correctly", async () => {
+  it("L1AssetRouter_AssetDeploymentTrackerRegistered is created correctly", async () => {
     // Processing the event
-    const mockDbUpdated = await L1AssetRouter.BridgehubDepositBaseTokenInitiated.processEvent({
+    const mockDbUpdated = await L1AssetRouter.AssetDeploymentTrackerRegistered.processEvent({
       event,
       mockDb,
     });
 
     // Getting the actual entity from the mock database
-    let actualL1AssetRouterBridgehubDepositBaseTokenInitiated = mockDbUpdated.entities.L1AssetRouter_BridgehubDepositBaseTokenInitiated.get(
+    let actualL1AssetRouterAssetDeploymentTrackerRegistered = mockDbUpdated.entities.L1AssetRouter_AssetDeploymentTrackerRegistered.get(
       `${event.chainId}_${event.block.number}_${event.logIndex}`
     );
 
     // Creating the expected entity
-    const expectedL1AssetRouterBridgehubDepositBaseTokenInitiated: L1AssetRouter_BridgehubDepositBaseTokenInitiated = {
+    const expectedL1AssetRouterAssetDeploymentTrackerRegistered: L1AssetRouter_AssetDeploymentTrackerRegistered = {
       id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
-      chainId: event.params.chainId,
-      from: event.params.from,
       assetId: event.params.assetId,
-      amount: event.params.amount,
+      additionalData: event.params.additionalData,
+      assetDeploymentTracker: event.params.assetDeploymentTracker,
     };
     // Asserting that the entity in the mock database is the same as the expected entity
-    assert.deepEqual(actualL1AssetRouterBridgehubDepositBaseTokenInitiated, expectedL1AssetRouterBridgehubDepositBaseTokenInitiated, "Actual L1AssetRouterBridgehubDepositBaseTokenInitiated should be the same as the expectedL1AssetRouterBridgehubDepositBaseTokenInitiated");
+    assert.deepEqual(actualL1AssetRouterAssetDeploymentTrackerRegistered, expectedL1AssetRouterAssetDeploymentTrackerRegistered, "Actual L1AssetRouterAssetDeploymentTrackerRegistered should be the same as the expectedL1AssetRouterAssetDeploymentTrackerRegistered");
   });
 });
